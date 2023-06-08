d1da40d24b6d2cb8441f70f6429782c89fefecc9
tevariou <teva.riou@gmail.com>
2023-06-09 01:11:55 +0200

    ci: add gh publish

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tevariou', // Usually your GitHub org/user name.
   projectName: 'atb', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
