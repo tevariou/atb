6e03985f309cb4f2596d9f3afa65ba132f0dece4
tevariou <teva.riou@gmail.com>
2025-02-28 21:09:44 +0100

    refactor: rm django api

--- api/atb/bike/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from atb.bike.models import Bike
-
-
-@admin.register(Bike)
-class BikeAdmin(admin.ModelAdmin):
-    list_filter = ["status"]
--- api/atb/bike/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-
-from atb.bike import models
-
-
-class BikeFilterSet(filters.FilterSet):
-    class Meta:
-        model = models.Bike
-        fields = ["status"]
