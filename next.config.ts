8d4ca0f3f6d409cab55a3fce0934663685fe6136
tevariou <teva.riou@gmail.com>
2025-07-08 17:42:59 +0200

    feat: deploy on gh pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/atb",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
